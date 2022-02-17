3ec0df188bb8d2066436a94bdd3d2d0ddf21138c
JDygard <jdygard@gmail.com>
2022-02-17 23:11:49 +0100

    add bag app views and urls

---
+++ bag/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from products.models import Product
+
+# Create your views here.
+def view_bag(request):
+    return render(request, "bag/bag.html")
+
+def add_to_bag(request, item_id):
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+    
+    request.session['bag'] = bag
+    return redirect(redirect_url)
+
+def adjust_bag(request):
+    print(request)
+
+def remove_from_bag(request):
+    print(request)
