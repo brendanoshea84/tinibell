70f31dd0e36e462b10267d3540b31e28698779bb
JDygard <jdygard@gmail.com>
2022-02-20 00:20:05 +0100

    Events: Add app views, models, urls

--- events/models.py
+++ events/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Events(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False)
+    label = models.CharField(max_length=50, blank=False, null=False)
+    description = models.TextField(max_length=255, blank=False, null=False)
+    image_1 = models.ImageField(upload_to="uploads/", blank=True, null=True)
--- events/views.py
+++ events/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Events
 
 # Create your views here.
+def events(request):
+    template = 'events/events.html'
+    events = Events.objects.all()
+    context = {
+        "events": events,
+    }
+    return render(request, template, context)
