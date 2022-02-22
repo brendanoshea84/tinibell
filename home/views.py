e774817a67cecf64f1516ae35444d4e78c2944a2
JDygard <jdygard@gmail.com>
2022-02-22 09:05:02 +0100

    add some splash to the splash page

--- home/views.py
+++ home/views.py
@@ -1,7 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render, HttpResponse
 from django.conf import settings
+from products.models import Product
 
 # Create your views here.
 def index(request):
-    return render(request, 'home/index.html')
+    products = Product.objects.filter(featured=True)
+    context = {
+        "products": products
+    }
+    return render(request, 'home/index.html', context)
